ff0bc1d46d7c5ca61255298578f2dc9424b1c1e4
Rom Vic <technoyguy@gmail.com>
2019-03-08 15:11:21 -0800

    ControlButton added

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,15 +1,42 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled,{css} from 'styled-components'
+
+const Logo = styled.div`
+  font-size:1.5em;
+  ` 
 const Bar= styled.div`
 display:grid;
 grid-template-columns:180px auto 100px 100px;
+margin-bottom:40px;
 ` 
 
-export default function AppBar() {
-  return   <Bar>
-    <div>CryptoDash</div>
-    <div> </div>
-    <div>Dashboard</div>
-    <div>Settings</div>
-  </Bar>
+const ControlButtonElem = styled.div`
+cursor:pointer;
+${props=>props.active && css`
+  text-shadow: 0px 0px 60px #03ff03;
+  `}
+`
+function toProperCase(lower){
+  return lower.charAt(0).toUpperCase() +lower.substr(1)
 }
+function ControlButton({name,active}){
+  return(
+    <ControlButtonElem active={active}>
+      {toProperCase(name)}
+    </ControlButtonElem>
+  )
+}
+
+
+export default function AppBar() {
+  return (
+     <Bar>
+        <div>CryptoDash</div>
+        <div> </div>
+        <ControlButton active name="dashboard"></ControlButton>
+        <ControlButton name="settings"></ControlButton>
+         
+     </Bar>
+  )
+}
+

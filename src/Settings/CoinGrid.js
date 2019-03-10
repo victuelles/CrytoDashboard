259f3db40440d13337c22c689ff166ed7615fd00
Rom Vic <technoyguy@gmail.com>
2019-03-10 13:39:29 -0700

    Add topsection coingrid

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -1,17 +1,28 @@
 import React from 'react'
 import {AppContext} from '../App/AppProvider'
 import styled,{css} from 'styled-components'
+import CoinTile from './CoinTile'
 
 export const CoinGridStyled = styled.div`
 display:grid;
-grid-template-columns:repeat(5,1fr)
+grid-template-columns:repeat(5,1fr);
+grid-gap:15px;
+margin-top:40px;
 `
+function getCoinsToDisplay(coinList,topSection){
+  return Object.keys(coinList).slice(0,topSection?10:100)
+}
 
-export default function() {
+export default function({topSection}) {
   return (<AppContext.Consumer>
 
     {({ coinList})=><CoinGridStyled>
-      {Object.keys(coinList).map(coinKey=><div key={coinKey}>{coinKey}</div>)}
+      {getCoinsToDisplay(coinList,topSection)
+      .map(coinKey=>(
+            <CoinTile key={coinKey} coinKey={coinKey} topSection={topSection}/>
+          )
+        )
+      }
     </CoinGridStyled>}
   </AppContext.Consumer>
   )
---
+++ src/Settings/CoinTile.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {AppContext} from "../App/AppProvider";
+import {SelectableTile, DisabledTile, DeletableTile} from "../Shared/Tile";
+import CoinHeaderGrid from './CoinHeaderGrid'
+import CoinImage from '../Shared/CoinImage'
+ 
+export default function({coinKey,topSection}){
+  return <AppContext.Consumer>
+    {({coinList }) => {
+      let coin = coinList[coinKey];
+
+      let TileClass = SelectableTile;
+      if(topSection){
+        TileClass=DeletableTile
+      }
+      console.log(topSection)
+      return (
+      <TileClass >
+        <CoinHeaderGrid topSection={topSection} name={coin.CoinName} symbol={coin.Symbol}/>
+        <CoinImage coin={coin}/>
+      </TileClass>
+      )
+    }}
+  </AppContext.Consumer>
+}
